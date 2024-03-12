ff54cddbde3798f912d31106eeecb8b4607de85d
Salraka <karla.ramirez.salud@gmail.com>
2024-03-11 19:08:28 -0600

    Actividad Clases playlist
    Para profesor alfonso

---
+++ src/main/java/com/mycompany/playlist/java/PlaylistJava.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.playlist.java;
+
+/**
+ *
+ * @author karla
+ */
+public class PlaylistJava {
+
+    public static void main(String[] args) {
+        Musicas p1=new Musicas("Elvira","Distorsion","2024",3);
+        p1.ShowPlaylist();
+    }
+}
